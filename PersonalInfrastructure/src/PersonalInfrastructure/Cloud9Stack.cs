c06f9cf1b8bbaecbf897de2751377a7367bf3e08
Scottie Enriquez <scottie@enriquez.xxx>
2023-02-17 22:17:53 -0800

    Adding owner ARN for Cloud9

--- PersonalInfrastructure/src/PersonalInfrastructure/Cloud9Stack.cs
+++ PersonalInfrastructure/src/PersonalInfrastructure/Cloud9Stack.cs
@@ -1,6 +1,7 @@
 using Amazon.CDK;
 using Amazon.CDK.AWS.Cloud9;
 using Amazon.CDK.AWS.EC2;
+using Amazon.CDK.AWS.SSM;
 using Constructs;
 
 namespace PersonalInfrastructure;
@@ -9,6 +10,9 @@ public class Cloud9Stack : Stack
 {
     internal Cloud9Stack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
     {
+        // SSM parameters
+        IStringParameter cloud9OwnerArn = StringParameter.FromStringParameterName(this, "Cloud9OwnerARN", "/personal-infrastructure-cdk/cloud9-owner-arn");
+        
         // networking
         Vpc cloud9Vpc = new Vpc(this, "Cloud9VPC", new VpcProps {
             IpAddresses = IpAddresses.Cidr("10.1.0.0/16"),
@@ -23,6 +27,7 @@ public class Cloud9Stack : Stack
             ImageId = "amazonlinux-2-x86_64",
             InstanceType = "m5.large",
             Name = "scottie-cloud9",
+            OwnerArn = cloud9OwnerArn.StringValue,
             // launching instance into public subnet for SSH
             SubnetId = cloud9Vpc.PublicSubnets[0].SubnetId
         });
